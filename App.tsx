b72c22814c8afab1e3e29879dafdfb3a29848783
Evandro oliveira <vandoaparecido@hotmail.com>
2022-07-19 20:01:40 -0300

    :hammer: :wrench: Configurando fonts personalizadas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { NativeBaseProvider, StatusBar } from "native-base";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "./src/components/Loading";
+
 import { THEME } from "./src/styles/theme";
 import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
