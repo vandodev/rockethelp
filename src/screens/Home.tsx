f11d559c72859b9547faaaf49cac80c5dba02b5d
Evandro oliveira <vandoaparecido@hotmail.com>
2022-07-23 20:12:20 -0300

    :hammer: :wrench: Implementando logout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 import {
   HStack,
   IconButton,
@@ -72,6 +74,15 @@ export function Home() {
   function handleOpenDetails(orderId: string) {
     navigation.navigate("details", { orderId });
   }
+
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch((error) => {
+        console.log(error);
+        return Alert.alert("Sair", "Não foi possível sair.");
+      });
+  }
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -84,7 +95,10 @@ export function Home() {
         px={6}
       >
         <Logo />
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
