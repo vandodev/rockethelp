77e00e8d9c996b2dd6f8cb941c594cda12992853
Evandro oliveira <vandoaparecido@hotmail.com>
2022-07-24 21:05:52 -0300

    :hammer: :wrench: Exibindo dethalhes da solicitação

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
-import { VStack, Text, Box } from "native-base";
+import { VStack, Text, HStack, useTheme, ScrollView, Box } from "native-base";
+import {
+  CircleWavyCheck,
+  Hourglass,
+  DesktopTower,
+  ClipboardText,
+} from "phosphor-react-native";
 import firestore from "@react-native-firebase/firestore";
 import { OrderFirestoreDTO } from "../DTOs/OrderFirestoreDTO";
 import { useRoute } from "@react-navigation/native";
 
+import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { OrderProps } from "../components/Order";
 import { Loading } from "../components/Loading";
 import { dateFormat } from "../utils/firestoreDateFormat";
+import { CardDetails } from "../components/CardDetails";
 
 type RouteParams = {
   orderId: string;
@@ -20,11 +28,11 @@ type OrderDetails = OrderProps & {
 };
 
 export function Details() {
-  const [solution, setSolution] = useState("");
   const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
   const [isLoading, setIsLoading] = useState(true);
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
+  const { colors } = useTheme();
 
   useEffect(() => {
     firestore()
@@ -75,7 +83,44 @@ export function Details() {
       <Box px={6} bg="gray.600">
         <Header title="Solicitação" />
       </Box>
-      <Text color="white">{orderId}</Text>
+
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {order.status === "closed" ? (
+          <CircleWavyCheck size={22} color={colors.green[300]} />
+        ) : (
+          <Hourglass size={22} color={colors.secondary[700]} />
+        )}
+
+        <Text
+          fontSize="sm"
+          color={
+            order.status === "closed"
+              ? colors.green[300]
+              : colors.secondary[700]
+          }
+          ml={2}
+          textTransform="uppercase"
+        >
+          {order.status === "closed" ? "finalizado" : "em andamento"}
+        </Text>
+      </HStack>
+
+      <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        <CardDetails
+          title="equipamento"
+          description={`Patrimônio ${order.patrimony}`}
+          icon={DesktopTower}
+        />
+
+        <CardDetails
+          title="descrição do problema"
+          description={order.description}
+          icon={ClipboardText}
+          footer={`Registrado em ${order.when}`}
+        />
+      </ScrollView>
+
+      {order.status === "open" && <Button title="Encerrar solicitação" m={5} />}
     </VStack>
   );
 }
